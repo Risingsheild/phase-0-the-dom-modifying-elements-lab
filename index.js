580acec12c0bab310ceb35b49e2d9ee216fdac0a
nickwells <wellsnick11@yahoo.com>
2022-01-21 19:55:02 -0800

    commit tree e0cc91ba05ef93c5fc7fc5f693c32a4e318f4c70

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ main.remove()
 const newHeader = document.createElement ("h1")
 console.log(newHeader);
 newHeader.id = "victory"
-newHeader.innerHTML = "is the champion"
+newHeader.innerHTML = "Nick is the champion"
